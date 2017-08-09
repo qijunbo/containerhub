a8facf24d87daa67979bd8cd9fc01829c27b7e46
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-09 11:03:19 +0800

    add  two methods to start a docker container  and  get it's port

--- src/main/java/com/example/docker/service/ShellExecutorService.java
+++ src/main/java/com/example/docker/service/ShellExecutorService.java
@@ -1,11 +1,8 @@
 package com.example.docker.service;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -15,48 +12,81 @@ import com.example.docker.repository.Container;
 @Configuration
 public class ShellExecutorService {
 
-	@Value("${command}")
-	private String commandPath;
+ 
+	@Value("${docker.create}")
+	private String createContainer;
 
 	public Container createContainer(String owner) {
 
-		if (execute(commandPath, owner)) {
-			return new Container("lims1", 8082);
+		if (execute(createContainer, owner).getErrorcode() == 0 ) {
+			return getPort(owner);
 		} else {
 			return null;
 		}
 	}
 
-	public boolean execute(String... command) {
+	@Value("${docker.getport}")
+	private String getport;
+	public Container getPort(String owner) {
+		ShellExecutorService.ShellResult result = execute(getport, owner);
+		if (result.getErrorcode()  == 0) {		
+			return new Container(owner, Integer.parseInt(result.getMessage()) );
+		} else {
+			return null;
+		}
+	}
 
-		ProcessBuilder pb = new ProcessBuilder(command );
-		//ProcessBuilder pb = new ProcessBuilder("notepad.exe", "test.txt");
+	public ShellResult execute(String... command) {
+		int exitVal = -1;
+		StringBuffer message = new StringBuffer();
+
+		ProcessBuilder pb = new ProcessBuilder(command);
+		// ProcessBuilder pb = new ProcessBuilder("notepad.exe", "test.txt");
 		// pb.command(new String[] { "notepad.exe", "test.txt" });
 		try {
 			Process process = pb.start();
-			InputStream stderr = process.getErrorStream();
-			InputStreamReader isr = new InputStreamReader(stderr);
-			BufferedReader br = new BufferedReader(isr);
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 			String line = null;
 			while ((line = br.readLine()) != null)
-				System.out.println(line);
-			int exitVal = process.waitFor();
-			System.out.println(exitVal);
+				message.append(line);
+			exitVal = process.waitFor();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		 
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+ 
 			e.printStackTrace();
 		}
 
-//		Map<String, String> env = pb.environment();
-//		env.put("VAR1", "myValue");
-//		env.remove("OTHERVAR");
-//		env.put("VAR2", env.get("VAR1") + "suffix");
-//		pb.directory(new File("myDir"));
+		// Map<String, String> env = pb.environment();
+		// env.put("VAR1", "myValue");
+		// env.remove("OTHERVAR");
+		// env.put("VAR2", env.get("VAR1") + "suffix");
+		// pb.directory(new File("myDir"));
 
-		return false;
+		return new ShellExecutorService.ShellResult(exitVal, message.toString());
+	}
+
+	static class ShellResult {
+
+		int errorcode;
+
+		String message;
+
+		public ShellResult(int errorcode, String message) {
+			super();
+			this.errorcode = errorcode;
+			this.message = message;
+		}
+
+		public int getErrorcode() {
+			return errorcode;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
 	}
 
 }
---
+++ src/main/resources/script/createcontainer.sh
@@ -0,0 +1,5 @@
+docker container prune -f
+docker run --name webapp -d -P -v ~/docker/webapp:/root/webapp qijunbo/java:8
+
+
+
