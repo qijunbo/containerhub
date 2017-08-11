fd6755da9ceb87d9d1238b73bfa7975b312b3867
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-11 16:09:03 +0800

    add some sample linux shell.

--- src/main/resources/script/createcontainer.sh
+++ src/main/resources/script/createcontainer.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
+name=$1
 docker container prune -f
-docker run --name webapp -d -P -v ~/docker/webapp:/root/webapp qijunbo/java:8
+docker run --name $name -d -P sunway/lims:1
 
-
-
+#docker run --name $name -d -P -v ~/docker/webapp:/root/webapp qijunbo/java:8
--- src/main/resources/script/getport.sh
+++ src/main/resources/script/getport.sh
@@ -1,1 +1,3 @@
- docker inspect --format='{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' webapp
+#!/bin/sh
+name=$1
+docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' $name
